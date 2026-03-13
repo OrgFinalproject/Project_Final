f67e3bd2407e259860417c8d91f5eded4463e485
Nadeen <s12340271@stu.najah.edu>
2026-03-13 17:10:53 +0200

    Add booking rules to BookingService

--- BookingService.java
+++ BookingService.java
@@ -1,11 +1,26 @@
 package soft;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookingService {
 
+    private List<BookingRuleStrategy> rules;
+    public BookingService() {
+    rules = new ArrayList<>();
+    rules.add(new DurationRule(30));
+    rules.add(new ParticipantLimitRule(5));
+
+    }
+
     public boolean bookAppointment(Appointment appointment) {
+        for (BookingRuleStrategy rule : rules) {
+            if (!rule.isValid(appointment)) {
+                System.out.println("Booking rejected: rule violated");
+                return false;
+            }
+        }
 
         appointment.setStatus("Confirmed");
-
         System.out.println("Appointment booked successfully!");
 
         return true;
