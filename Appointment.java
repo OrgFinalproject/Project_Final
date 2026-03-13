0003bb2bbe9721049fc131ee34201744bd20fc7b
Nadeen <s12340271@stu.najah.edu>
2026-03-13 16:48:56 +0200

    Add duration and participants to Appointment class

--- Appointment.java
+++ Appointment.java
@@ -5,12 +5,16 @@ public class Appointment {
     private String date;
     private String time;
     private String status;
+    private int participants;
+    private int duration;
     // TODO: إضافة المزيد من الخصائص إذا لزم الأمر 
 
-    public Appointment(String date, String time) {
+    public Appointment(String date, String time,int duration,int participants) {
         this.date = date;
         this.time = time;
-        this.status = "Pending";   
+        this.status = "Pending";  
+        this.duration=duration;
+        this.participants=participants;
     }
 
 
@@ -23,6 +27,12 @@ public class Appointment {
     public void setStatus(String status) {
         this.status = status;
     }
+    public int getDuration(){
+        return duration;
+    }
+    public int getParticipants(){
+        return participants;
+    }
 
     public String getStatus() {
         return status;
@@ -32,6 +42,9 @@ public class Appointment {
     public void displayInfo() {
         System.out.println("Date: " + date);
         System.out.println("Time: " + time);
+        System.out.println("Duration "+duration);
+        System.out.println("Participants "+participants);
         System.out.println("Status: " + status);
+        
     }
 }
