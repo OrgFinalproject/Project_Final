a1a9a05edbaf8cbe3168e02ab89c5ae6c82b9b5b
Tala Jaber <s12324406@stu.najah.edu>
2026-03-17 01:12:57 -0700

    adding list of appointments

--- TimeSlot.java
+++ TimeSlot.java
@@ -3,13 +3,16 @@ package software;
 
 
 import java.time.LocalTime;
+import java.util.List;
+
+import soft.Appointment;
 
 public class TimeSlot {
 	
 	private LocalTime startTime;
     private LocalTime endTime;
     private boolean isAvailable;
-    
+    private List< Appointment>  Appointments;
     
     public TimeSlot(LocalTime startTime, LocalTime endTime) {
     	  if (endTime.isBefore(startTime)) {
@@ -31,6 +34,18 @@ public class TimeSlot {
         return this.isAvailable;
     }
        
+    
+    public boolean addAppointment(Appointment appointment)
+    {
+    	Appointments.add(appointment);
+    	return true;
+    }
+    public long getRemainingTime() {
+        long totalBookedMinutes = Appointments.stream()
+                                               .mapToLong(Appointment::getDuration)
+                                               .sum();
+        return java.time.Duration.between(startTime, endTime).toMinutes() - totalBookedMinutes;
+    }
     public void book() {
         if (isAvailable) {
             isAvailable = false;
