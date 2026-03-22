1cd2ff3de6ff8a2fa0ecacb0d7e731b18f6c2ffe
Talabarhoush <engtalabarhoush@gmail.com>
2026-03-23 01:01:21 +0200

    Enhance validation for urgent appointment rules
    Added validation for duration and participants in urgent appointments.

--- UrgentRule.java
+++ UrgentRule.java
@@ -4,9 +4,17 @@ public class UrgentRule implements BookingRuleStrategy {
 
     @Override
     public boolean isValid(Appointment app) {
-        if (app.getType() == AppointmentType.URGENT) {
-            return true;
+
+        if (app.getDuration() > 30) {
+            System.out.println("Urgent appointment must be <= 30 minutes");
+            return false;
         }
+
+        if (app.getParticipants() > 2) {
+            System.out.println("Urgent appointment max 2 participants");
+            return false;
+        }
+
         return true;
     }
 }
